bcda529e3b60dfb2595618722992fb9ad811e9c1
Viresh Patel <viresh.p2003@gmail.com>
2026-02-23 16:03:05 -0500

    Backend setup

---
+++ summs-backend/src/main/java/com/team/summs_backend/SummsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.team.summs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummsBackendApplication.class, args);
+	}
+
+}
